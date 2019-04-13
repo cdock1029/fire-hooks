1ce0bc319220ee3f6c98bf8e60dc50fe159e95b3
Conor Dockry <conordockry@gmail.com>
2019-04-13 18:14:02 -0400

    simplify types

--- src/index.ts
+++ src/index.ts
@@ -5,101 +5,86 @@ import * as React from 'react'
 import {} from 'firebase/app'
 const { useState, useEffect } = React
 
-export interface CollectionResult {
-  error?: Error
-  snap?: firebase.firestore.QuerySnapshot
-}
 export function useCollection(
   queryBuilder: () => firebase.firestore.Query | undefined,
   deps: any[] = [],
   { get } = { get: false },
-): CollectionResult {
-  const [state, setState] = useState({
-    error: undefined,
-    snap: undefined,
-  } as CollectionResult)
+): firebase.firestore.QuerySnapshot | undefined {
+  const [snap, setSnap] = useState<
+    firebase.firestore.QuerySnapshot | undefined
+  >(undefined)
   useEffect(() => {
     const query = queryBuilder()
     if (query) {
       if (get) {
         query
           .get()
-          .then(snap => setState({ error: undefined, snap }))
-          .catch(error => setState({ error, snap: undefined }))
+          .then(snap => setSnap(snap))
+          .catch(error => {
+            setSnap(undefined)
+            throw error
+          })
         return
       }
       const unsub = query.onSnapshot(
-        snap => setState({ error: undefined, snap }),
-        error => setState({ error, snap: undefined }),
+        snap => setSnap(snap),
+        error => {
+          setSnap(undefined)
+          throw error
+        },
       )
       return () => (console.log('unsub col'), unsub())
     } else {
-      setState({ error: undefined, snap: undefined })
+      setSnap(undefined)
       return
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [...deps, get])
-  return state
+  return snap
 }
 
-export interface CollectionDataResult<T extends object> {
-  error?: Error
-  data?: Array<T & { id: string }>
-}
 export function useCollectionData<T extends object>(
   queryBuilder: () => firebase.firestore.Query | undefined,
   deps: any[] = [],
-): CollectionDataResult<T> {
-  const { error, snap } = useCollection(queryBuilder, deps)
-  return {
-    error,
-    data: snap
-      ? snap.docs.map(d => ({ id: d.id, ...(d.data() as T) }))
-      : undefined,
-  }
+): Array<T & { id: string }> | undefined {
+  const snap = useCollection(queryBuilder, deps)
+  return snap
+    ? snap.docs.map(d => ({ id: d.id, ...(d.data() as T) }))
+    : undefined
 }
 
-interface DocumentResult {
-  error?: Error
-  snap?: firebase.firestore.DocumentSnapshot
-}
 export function useDocument(
   refBuilder: () => firebase.firestore.DocumentReference | undefined,
   deps: any[] = [],
-): DocumentResult {
-  const [state, setState] = useState({
-    error: undefined,
-    snap: undefined,
-  } as DocumentResult)
+): firebase.firestore.DocumentSnapshot | undefined {
+  const [snap, setSnap] = useState<
+    firebase.firestore.DocumentSnapshot | undefined
+  >(undefined)
   useEffect(() => {
     const ref = refBuilder()
     if (ref) {
       const unsub = ref.onSnapshot(
-        snap => setState({ error: undefined, snap }),
-        error => setState({ error, snap: undefined }),
+        snap => setSnap(snap),
+        error => {
+          setSnap(undefined)
+          throw error
+        },
       )
       return () => (console.log('unsub doc'), unsub())
     } else {
-      setState({ error: undefined, snap: undefined })
+      setSnap(undefined)
     }
     return
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps)
-  return state
+  return snap
 }
 
-type DocumentDataResult<T extends object> =
-  | T & { id: string }
-  | null
-  | undefined
 export function useDocumentData<T extends object>(
   refBuilder: () => firebase.firestore.DocumentReference | undefined,
   deps: any[] = [],
-): DocumentDataResult<T> {
-  const { error, snap } = useDocument(refBuilder, deps)
-  if (error) {
-    throw error
-  }
+): T & { id: string } | null | undefined {
+  const snap = useDocument(refBuilder, deps)
   return snap
     ? snap.exists
       ? { id: snap.id, ...(snap.data() as T) }
